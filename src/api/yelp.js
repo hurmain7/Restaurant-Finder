f2b8c205deaeb997fb8480d4a2edab2f23b8f70f
Hurmain Khalid <46092815+hurmain7@users.noreply.github.com>
2020-11-12 03:15:20 +0500

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    Authorization: 'Bearer 0iZ-BlyHQWl3BpReNbvMjBNfIz2XmW5Q7FgFsbF2bL8f8Og9FAWeOvMdrpjH7FiOzRyajyzAVcbvBdHJl99-1S_OWheT6R2I4vzX5-C935-_FKZlB2XcuwV6tXunXXYx'
+    Authorization: 'auth token'
   }
 });
